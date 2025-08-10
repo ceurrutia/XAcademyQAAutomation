319d64337a37541a4d7e894666a644e9d605b713
Cecilia urrutia <urrutia.cici@gmail.com>
2025-08-10 19:08:11 -0300

    agrego lib mochawesome para generar reportes, creo scripts para automatizarlos

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      //listeners que tenga
     },
-  },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,         //No sobreescribe archivo
+      html: false,               //Un solo json
+      json: true,
+      reportFilename: "[name].json" //crea uno para cada
+    }
+  }
 });
+
