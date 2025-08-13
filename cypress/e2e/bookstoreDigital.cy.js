9b34142c61ff1fc2fe0d469255d2612be2655a39
Cecilia urrutia <urrutia.cici@gmail.com>
2025-08-13 17:36:52 -0300

    corrijo test bookstoreDigital, encontrando el item de la nav

--- cypress/e2e/bookstoreDigital.cy.js
+++ cypress/e2e/bookstoreDigital.cy.js
@@ -1,5 +1,9 @@
-describe('abrir sitio web Bookstore digital', () => {
-  it('passes', () => {
-    cy.visit('https://bookstorefrontend-henna.vercel.app/digital-books')
+describe('Abre pestania Bookstore digital', () => {
+  it('Deberia hacer click sobre la pestania Digital Books y direccionar ', () => {
+    cy.visit('https://bookstorefrontend-henna.vercel.app')
+    //click
+    cy.contains('.nav-link', 'Digital Books').click();
+    cy.url().should('include', 'bookstorefrontend-henna.vercel.app');
   })
+   
 })
